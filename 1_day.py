a373823b96ebacd54446f1a2225b7cf8f51bccec
satishsolanki1990 <satishsolanki1990@gmail.com>
2020-06-07 21:35:47 -0700

    Adding Comments only

--- 1_day.py
+++ 1_day.py
@@ -1,6 +1,27 @@
-import pandas
-import numpy
+import numpy as np
+import pandas as pd
 
 # read the data
+df=pd.read_csv('Data_day1.csv')
 
-#
+# Preprocessing:
+# 1. Histogram : to see the data distribution
+
+# 2. Data visualization
+
+# 3. missing data: there are two ways (1) delete that raw (2) use mean variance to assign value
+
+# 4. Categorical data encoder
+
+
+# 5. feature Scaling or  Normalization: there are many ways to normalize, every technique has
+# its limitation and strength. Following techniques are usually followed
+#   (1). Max-min normalization
+#   (2). mean/variance normalization or mean/std normalization
+#   (3). log normalization
+#   (4).
+
+
+# 6. feature selection : based on histogram or Heatmap
+
+# 7. dataset split (test/train/val)
