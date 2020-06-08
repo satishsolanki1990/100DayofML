e52d581b413bff2bf2c3aeb20a99e1a72e895583
satishsolanki1990 <satishsolanki1990@gmail.com>
2020-06-07 22:03:22 -0700

    Adding Comments only

--- 1_day.py
+++ 1_day.py
@@ -1,15 +1,26 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # read the data
 df=pd.read_csv('Data_day1.csv')
+# # Preprocessing:
+# print('name of columns = ',df.columns)
+# print('overall shape of dataframe ',df.shape)
+# print('top 5 rows of dataset ',df.head(n=5))
+#
+# # 1. Statical Analysis:  compute mean/variance
+# print('mean = ',df.mean())
+# print('standard deviation = ',df.std())
+# print('variance = ', df.var())
 
-# Preprocessing:
-# 1. Histogram : to see the data distribution
-
-# 2. Data visualization
+# 2. Data visualization: Histogram : to see the data distribution
+plt.hist=df.hist(bins=3)
+plt.show()
 
 # 3. missing data: there are two ways (1) delete that raw (2) use mean variance to assign value
+
 
 # 4. Categorical data encoder
 
@@ -24,4 +35,4 @@ df=pd.read_csv('Data_day1.csv')
 
 # 6. feature selection : based on histogram or Heatmap
 
-# 7. dataset split (test/train/val)
+# 8. dataset split (test/train/val)
